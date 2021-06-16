94207afe16ddc3e4bd8209ed6a2b0ab6c6206432
Mike <mikewrosey@gmail.com>
2021-06-16 18:16:57 -0400

    add OpenNMS light/dark logo

--- web/navbar/src/App.vue
+++ web/navbar/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <Menubar :model="items" />
+  <Menubar>
+    <template #start>
+        <Logo />
+    </template>
+
+    <template #end>
+        <Button>Return to previous UI</Button>
+    </template>
+  </Menubar>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
+import Logo from './assets/LogoLightBG.vue'
 
 export default defineComponent({
   components: {
-    Menubar
-  },
-  data() {
-    return {
-      items: [
-        {
-          label: 'Return to previous UI',
-          icon: 'pi pi-fw pi-arrow-left'
-        }
-      ]
-    }
+    Menubar,
+    Button,
+    Logo
   }
 })
 </script>
 
-<style>
-</style>
---
+++ web/navbar/src/assets/LogoDarkBG.vue
@@ -0,0 +1,20 @@
+<template>
+  <img :src="logo" class="img"/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logo: require('./OpenNMS_Horizontal-Logo_Dark-BG.svg')
+    }
+  }
+}
+</script>
+
+<style>
+  .img {
+    widows: 8em;
+    height: 2em;
+  }
+</style>
