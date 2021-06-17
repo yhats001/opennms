792461317e9273ce51d3514a1593d0fb2448bb0c
Mike <mikewrosey@gmail.com>
2021-06-17 15:03:37 -0400

    add baseURL for missing env var

--- web/nodes/src/services/axiosInstance.ts
+++ web/nodes/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const portal = axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL,
+  baseURL: process.env.VUE_APP_BASE_URL || '/opennms/api/v2',
   withCredentials: true
 })
 
