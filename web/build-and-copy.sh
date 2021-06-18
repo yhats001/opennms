3adf6690b75eebd958017ab3987632d757b9a2fc
Jesse White <jesse@opennms.org>
2021-06-17 22:58:09 -0400

    build a container with the new spa

--- web/build-and-copy.sh
+++ web/build-and-copy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 UI_PATH="${OPENNMS_HOME}/jetty-webapps/opennms/assets/ui"
-echo "Rebuilding new UI structure in: $UI_PATH"
-rm -rf $UI_PATH/*
+if [ -n "$1" ]; then
+  UI_PATH="$1"
+fi
+
+yarn build
+
+echo "Copy ew UI structure in: $UI_PATH"
 cp -R root-config/dist/* "${UI_PATH}/"
 cp -R navbar/dist "${UI_PATH}/navbar"
 cp -R nodes/dist "${UI_PATH}/nodes"
